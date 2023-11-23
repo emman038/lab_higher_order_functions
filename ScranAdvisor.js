85f45584303f7f6354e7b65ce3c78fa6c2db6452
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-11-23 01:52:17 +0000

    Task completed

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -2,6 +2,21 @@ const ScranAdvisor = function(restaurants){
     this.restaurants = restaurants;
 }
 
-// Add methods to prototype
+ScranAdvisor.prototype.countRestaurants = function (){
+    return this.restaurants.length;
+};
+
+ScranAdvisor.prototype.findRestaurantByName = function (restaurantName){
+    return this.restaurants.find((restaurant) => { return restaurant.name == restaurantName});
+};
+
+ScranAdvisor.prototype.findAllRestaurantNames = function (){
+    return this.restaurants.map((restaurant)=>{return restaurant.name});
+};
+
+ScranAdvisor.prototype.findAllByLocation = function (townName){
+    return this.restaurants.filter((restaurant)=>{return restaurant.location.town === townName})
+}
+
 
 module.exports = ScranAdvisor;
--- ScranAdvisor.test.js
+++ ScranAdvisor.test.js
@@ -13,13 +13,13 @@ describe('ScranAdvisor', () => {
         expect(scranAdvisor.restaurants).not.toBeNull();
     })
 
-    test.skip('can count the number of restaurants', () => {
+    test('can count the number of restaurants', () => {
         const expected = 23;
-        const actual = null;
+        const actual = scranAdvisor.countRestaurants();
         expect(actual).toBe(expected);
     })
 
-    test.skip('can find Happy Lamb Hot Pot restaurant by full name', () => {
+    test('can find Happy Lamb Hot Pot restaurant by full name', () => {
         const expected = {
             "id": 8,
             "name": "Happy Lamb Hot Pot",
@@ -37,11 +37,11 @@ describe('ScranAdvisor', () => {
             "latitude": 52.475159788534754,
             "longitude": -1.8966440079643896
         };
-        const actual = null;
-        expect(actual).toBe(expected);
+        const actual = scranAdvisor.findRestaurantByName("Happy Lamb Hot Pot");
+        expect(actual).toEqual(expected);
     })
 
-    test.skip('can find the name of all restaurants', () => {
+    test('can find the name of all restaurants', () => {
         const expected = [
             'Oro',
             'Halloumi South',
@@ -67,11 +67,11 @@ describe('ScranAdvisor', () => {
             "Gopal's Corner Victoria",
             'The Gordon Highlander'
           ]
-          const actual = null;
-          expect(actual).toBe(expected);
+          const actual = scranAdvisor.findAllRestaurantNames();
+          expect(actual).toEqual(expected);
     })
 
-    test.skip('can find all restaurants from Glasgow', () => {
+    test('can find all restaurants from Glasgow', () => {
         const expected = [
             {
               id: 1,
@@ -118,8 +118,8 @@ describe('ScranAdvisor', () => {
               longitude: -4.255168672861043
             }
           ];
-          const actual = null;
-          expect(actual).toBe(expected);
+          const actual = scranAdvisor.findAllByLocation("Glasgow");
+          expect(actual).toEqual(expected);
     })
 
     // Extensions
